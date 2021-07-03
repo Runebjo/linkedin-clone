f763881e5c1eb866eca776479435869d0e559f82
Rune <rune.bjorsnos@gmail.com>
2021-07-03 10:34:31 +0200

    ADD header search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Lets build a LinkedIn CLONE</h1>
+    <div className="app">
+      <Header />
+      
     </div>
   );
 }
