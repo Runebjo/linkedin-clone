84d60f57b4739df3622464bd69ba7b7913c176f8
Rune <rune.bjorsnos@gmail.com>
2021-07-06 19:08:37 +0200

    ADD sidebar

---
+++ src/Siderbar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import "./Sidebar.css"
+import { Avatar } from '@material-ui/core';
+
+function Sidebar() {
+    const recentItem = (topic) => (
+        <div className="sidebar__recentItem">
+            <span className="sidebar__hash">#</span>
+            <p>{topic}</p>
+        </div>
+    )
+
+    return (
+        <div className="sidebar">
+            <div className="sidebar__top">
+                <img src="https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/p-s45-ae-object-026-l.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=0cb3a1e5dbe756bd7ecb66da8064e25a" alt="" />
+                <Avatar className="sidebar__avatar" />
+                <h2>Rune Bjørsnøs</h2>
+                <h4>rune.bjorsnos@gmail.com</h4>
+            </div>
+            <div className="sidebar__stats">
+                <div className="sidebar__stat">
+                    <p>Who viewed you</p>
+                    <p className="sidebar__statNumber">2,543</p>
+                </div>
+                <div className="sidebar__stat">
+                    <p>Views on post</p>
+                    <p className="sidebar__statNumber">2,448</p>
+                </div>
+            </div>
+            <div className="sidebar__bottom">
+                <p>Recent</p>
+                {recentItem('reactjs')}
+                {recentItem('programming')}
+                {recentItem('softwareengineering')}
+                {recentItem('design')}
+                {recentItem('developer')}
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
